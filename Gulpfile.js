571c9d637f8b95d547dcc722157942a9530befdf
Karuto <iamkaruto@gmail.com>
2017-10-26 22:44:00 +0100

    Simplify gulp styles

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('css/*.css')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('dist/app.css'));
 });
 
 // TODO: I was in the middle of setting up Gulp and Sass, but my machine's NPM doesn't work.
