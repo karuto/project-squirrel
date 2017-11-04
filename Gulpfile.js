9eb85fb2485628a5236faa47e4abc5ffb0f85f16
Karuto <iamkaruto@gmail.com>
2017-11-04 23:16:47 +0000

    Made gulp work

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,10 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
     gulp.src('css/*.css')
-        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/app.css'));
 });
 
 // TODO: I was in the middle of setting up Gulp and Sass, but my machine's NPM doesn't work.
-http://ryanchristiani.com/getting-started-with-gulp-and-sass/
-https://css-tricks.com/gulp-for-beginners/
+// http://ryanchristiani.com/getting-started-with-gulp-and-sass/
+// https://css-tricks.com/gulp-for-beginners/
