d78467b5fa4785dea196e8b927362e1b811087ef
Karuto <iamkaruto@gmail.com>
2017-11-10 23:23:07 +0000

    Switch absolute path from root dir in gulp setup

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 
 gulp.task('default', function() {
-    gulp.src('css/*.css')
+    gulp.src('./css/*.css')
         .pipe(concat('app.min.css'))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('./dist/css/'));
 });
 
 // TODO: I was in the middle of setting up Gulp and Sass, but my machine's NPM doesn't work.
