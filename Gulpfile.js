b199ab0e57448a57c90aa1722cf64adc092fba7f
Karuto <iamkaruto@gmail.com>
2017-11-11 01:06:53 +0000

    Remove comments

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,3 @@ gulp.task('default', function() {
         .pipe(concat('app.min.css'))
         .pipe(gulp.dest('./dist/css/'));
 });
-
-// TODO: I was in the middle of setting up Gulp and Sass, but my machine's NPM doesn't work.
-// http://ryanchristiani.com/getting-started-with-gulp-and-sass/
-// https://css-tricks.com/gulp-for-beginners/
