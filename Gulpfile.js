c7ba6cf8d55f81ffc5bcaedf0fdf606a495608ac
Karuto <iamkaruto@gmail.com>
2017-11-21 01:15:54 +0000

    Changed to minify var

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var cleanCSS = require('gulp-clean-css');
+var minifyCSS = require('gulp-clean-css');
 
 gulp.task('default', function() {
   gulp.src('./css/*.css')
     .pipe(concat('app.min.css'))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(minifyCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./dist/css/'));
 });
