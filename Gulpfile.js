021d5f80109f3f8edb84b75091c62f96c91f9a01
Karuto <iamkaruto@gmail.com>
2017-11-21 01:14:57 +0000

    Added minifier to ie8

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,5 +5,6 @@ var cleanCSS = require('gulp-clean-css');
 gulp.task('default', function() {
   gulp.src('./css/*.css')
     .pipe(concat('app.min.css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./dist/css/'));
 });
