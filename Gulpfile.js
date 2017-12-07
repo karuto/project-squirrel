3cf7af12af5c087d6cb5add802da6cdd3cd69553
Karuto <iamkaruto@gmail.com>
2017-12-07 20:12:38 +0000

    Removed debugging

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,5 @@ var minifyCSS = require('gulp-clean-css');
 gulp.task('default', function() {
   gulp.src('./css/*.css')
     .pipe(concat('app.min.css'))
-    .pipe(minifyCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./dist/css/'));
 });
